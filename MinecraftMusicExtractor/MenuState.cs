e22c4493095e0fed9d17eca6e8ef0b83caff6674
Eric Butler <e.c.butler@outlook.com>
2023-12-10 00:35:22 -0500

    Open index json file and read contents

---
+++ MinecraftMusicExtractor/MenuState.cs
@@ -0,0 +1,9 @@
+/// <summary>
+/// Manages the state of the menu selections
+/// </summary>
+class MenuState
+{
+    public bool ExtractMusic { get; set; }
+    public bool ExtractSounds { get; set; }
+
+}
--- MinecraftMusicExtractor/Program.cs
+++ MinecraftMusicExtractor/Program.cs
@@ -1,5 +1,167 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Runtime;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+// See https://aka.ms/new-console-template for more information
+// Set how many menu items there are
+const int NUM_MENU_ITEMS = 2;
+var _selectionIndex = 0;
+
 Console.WriteLine("Minecraft Music Extractor");
-Console.WriteLine("Enter location of .minecraft folder. Example: home/.minecraft");
-var minecraftDir = Console.ReadLine();
-Console.WriteLine(minecraftDir);
+// Create a menu state object to track user's selection
+var menuState = new MenuState()
+{
+    ExtractMusic = true
+};
+
+// Prompt user for the minecraft folder
+string? _minecraftDir = null;
+while (_minecraftDir == null)
+{
+    Console.Write("Enter location of .minecraft folder. (Example: home/.minecraft): ");
+    _minecraftDir = Console.ReadLine();
+}
+
+// Once we have the minecraft directory, look into the assets folder and find the indexes. This keeps a
+// maooing of resource names and hashes. We need to look up the resource name and locate the file by
+// its hash.
+// TODO: Look up asset indexes.
+
+// Once we have the indexes let user decide what files to extract.
+// Present the menu.
+PrintMainMenu(menuState);
+
+// Start main loop.
+ConsoleKeyInfo _key = default;
+while (_key.Key != ConsoleKey.Escape)
+{
+    _key = Console.ReadKey(true);
+
+    // If key is down arrow, increase selection
+    if (_key.Key == ConsoleKey.DownArrow)
+    {
+        MoveSelectionDown();
+    }
+    else if (_key.Key == ConsoleKey.UpArrow)
+    {
+        MoveSelectionUp();
+    }
+    else if (_key.Key == ConsoleKey.Spacebar)
+    {
+        SetSelection(menuState);
+
+    }
+    else if (_key.Key == ConsoleKey.Enter)
+    {
+        // Validate and proceed.
+        await LoadIndexes();
+        break;
+    }
+
+    // Move back to the top of the menu
+    Console.CursorTop -= NUM_MENU_ITEMS;
+
+    // Print the menu again
+    PrintMainMenu(menuState);
+}
+
+//
+void PrintMainMenu(MenuState menuState)
+{
+    // Print each menu option and update with selection
+    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.Write($"{GetSelectionCursor(0)} ");
+    Console.ForegroundColor = ConsoleColor.Gray;
+    Console.Write($"[{GetSelectionCharacter(menuState.ExtractMusic)}] Extract music");
+    Console.WriteLine();
+
+    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.Write($"{GetSelectionCursor(1)} ");
+    Console.ForegroundColor = ConsoleColor.Gray;
+    Console.Write($"[{GetSelectionCharacter(menuState.ExtractSounds)}] Extract sounds");
+    Console.WriteLine();
+
+}
+
+void SetSelection(MenuState menuState)
+{
+    switch (_selectionIndex)
+    {
+        case 0:
+            menuState.ExtractMusic = !menuState.ExtractMusic;
+            break;
+        case 1:
+            menuState.ExtractSounds = !menuState.ExtractSounds;
+            break;
+    }
+}
+
+static string GetSelectionCharacter(bool value)
+{
+    // Return an x if true or space if false
+    return value ? "x" : " ";
+}
+
+string GetSelectionCursor(int menuIndex)
+{
+    return _selectionIndex == menuIndex ? ">" : " ";
+}
+
+void MoveSelectionUp()
+{
+    // Increase selection index
+    _selectionIndex--;
+    if (_selectionIndex < 0)
+    {
+        // Go to the end
+        _selectionIndex = NUM_MENU_ITEMS - 1;
+    }
+}
+
+void MoveSelectionDown()
+{
+    // Increase selection index
+    _selectionIndex++;
+    if (_selectionIndex > NUM_MENU_ITEMS - 1)
+    {
+        // Go back to beginning
+        _selectionIndex = 0;
+    }
+}
+
+async Task LoadIndexes()
+{
+    Console.WriteLine("\nChecking indexes...");
+
+    var assetsFolder = Path.Combine(_minecraftDir, "assets");
+    var indexesFolder = Path.Combine(assetsFolder, "indexes");
+    if (Directory.Exists(indexesFolder))
+    {
+        // Look up the .json files in the folder.
+        var indexes = Directory.EnumerateFiles(indexesFolder, "*.json");
+        Console.WriteLine("Reading indexes...");
+        foreach (var index in indexes)
+        {
+            // Read the index and deserialize the json file.
+            var indexJson = await File.ReadAllTextAsync(index);
+            // Deserialize to a JObject
+            var objectIndex = (JObject?)JsonConvert.DeserializeObject(indexJson);
+            if (objectIndex != null)
+            {
+                // Add the indexes to a dictionary
+                var assets = objectIndex["objects"]?.Children();
+                Console.WriteLine(assets);
+                if (assets != null)
+                {
+                    foreach (var asset in assets)
+                    {
+                        Console.WriteLine(asset.Value<JProperty>()?.Name);
+                    }
+                }
+
+
+            }
+        }
+    }
+}
+
